4a28e70fca6d98f08ad9fe0273af6d3fa6c24c0c
Yeferal <yefercuber20@gmailcom>
2021-04-21 15:14:39 -0600

    se modifica el archivo obtenerDatosUsuario.php

--- Web/backend/obtenerDatosUsuario.php
+++ Web/backend/obtenerDatosUsuario.php
@@ -3,9 +3,9 @@
 
 use function PHPSTORM_META\type;
 
-session_start();
+//session_start();
 $email = $_SESSION['correo'];
-$root = $_SERVER['DOCUMENT_ROOT'] . '/Tiempo_Maya_Web/';
+$root = $_SERVER['DOCUMENT_ROOT'] . '/Proyecto-Maya/Web/';
 $connection = include '../conexion/conexion.php';
 // Consulta segura para evitar inyecciones SQL.
 
@@ -19,8 +19,10 @@ $contribution_number = $connection->query("SELECT count(id) as acontecimientos F
 $number_con = mysqli_fetch_assoc($contribution_number);
 $number = $number_con['acontecimientos'];
 $fecha = $user['fechaNacimiento'];
-$nahual = include $root . '../backend/buscar/conseguir_nahual_nombre.php';
-$energia = include $root . '../backend/buscar/conseguir_energia_numero.php';
+// $nahual = include $root . '../backend/buscar/conseguir_nahual_nombre.php';
+// $energia = include $root . '../backend/buscar/conseguir_energia_numero.php';
+$nahual = include '../backend/buscar/conseguir_nahual_nombre.php';
+$energia = include '../backend/buscar/conseguir_energia_numero.php';
 $imagen = $connection->query("SELECT imagen FROM tiempomaya.usuario WHERE correo='" . $email . "' and imagen is not null;");
 if ($imagen->num_rows == 1) {
   $img = mysqli_fetch_assoc($imagen);
